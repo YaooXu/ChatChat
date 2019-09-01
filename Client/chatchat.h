dfc212e8498edf200acd44b717a2a7f6d324ace8
wanghuazhang <1431310702@qq.com>
2019-09-01 14:45:28 +0800

    初始化客户端代码-whz

---
+++ Client/chatchat.h
@@ -0,0 +1,54 @@
+#ifndef CHATCHAT_H
+#define CHATCHAT_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QMessageBox>
+#include <QTcpSocket>
+#include <QByteArray>
+#include <QListView>
+#include <QListWidget>
+#include <QLabel>
+#include <QStackedWidget>
+#include <QButtonGroup>
+#include <QDebug>
+#include <QMessageBox>
+#include <QTcpSocket>
+#include <QByteArray>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QToolButton>
+#include <QToolBox>
+#include <QGroupBox>
+#include <QLineEdit>
+#include <QTextBrowser>
+
+
+#include"login.h"
+
+namespace Ui {
+class Chatchat;
+}
+
+class Chatchat : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Chatchat(QWidget *parent = nullptr);
+    ~Chatchat();
+
+private:
+    Ui::Chatchat *ui;
+    QTcpSocket *p_sock;
+
+    QVBoxLayout *left_Layout;
+    QPushButton *user_Button;
+    QPushButton *message_Button;
+    QPushButton *friend_Button;
+    QPushButton *group_Button;
+    QPushButton *setting_Button;
+};
+
+#endif // CHATCHAT_H
---
+++ Client/login.h
@@ -0,0 +1,49 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QDialog>
+#include <QDebug>
+#include <QMessageBox>
+#include <QTcpSocket>
+#include <QByteArray>
+
+#include "chatchat.h"
+#include "main_weight.h"
+
+namespace Ui {
+class Login;
+}
+
+class Login : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Login(QWidget *parent = nullptr);
+    ~Login();
+
+    bool islogin;
+    void init_Login();
+    int userid;
+    QString passwd;
+
+private slots:
+    void loginBtnOnclick();
+    void logoutBtnOnclick();
+    void handconnect();
+    void handData();
+
+private:
+    Ui::Login *ui;
+
+    QTcpSocket sock;
+
+    QLabel *lable0;
+    QLabel *lable1;
+    QLineEdit *lineEditUserID;
+    QLineEdit *lineEditPasswd;
+    QPushButton *loginBtn;
+    QPushButton *logoutBtn;
+};
+
+#endif // LOGIN_H
